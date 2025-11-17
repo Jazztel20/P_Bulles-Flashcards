76781e77f3ec5da0092e6747578ef3b050998a43
Jazztel20 <pb58unc@eduvaud.ch>
2025-11-17 08:59:52 +0100

    fix(chemin): il y a avait un dossier en trop dans le chemin du projet

--- P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/app/models/teacher.ts
+++ P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/app/models/teacher.ts
@@ -1,39 +1,28 @@
 import { DateTime } from 'luxon'
-import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
-import Section from './section.js'
+import { BaseModel, column, belongsTo, BelongsTo } from '@adonisjs/lucid/orm'
+import Section from '#models/section'
 
 export default class Teacher extends BaseModel {
-  // renommer le nom de la table pour respecter les conventions de nommage de l'ETML
-
+  // Renommer le nom de la table pour respecter les conventions de nommage de l'ETML
   public static table = 't_teacher'
-
   @column({ isPrimary: true })
   declare id: number
-
   @column()
   declare gender: String
-
   @column()
   declare firstname: String
-
   @column()
   declare lastname: String
-
   @column()
   declare nickname: String
-
   @column()
   declare origine: String
-
   @column()
-  declare sectionId: number // colonne correspondant à la clé étrangère
-
+  declare sectionId: number // Colonne correspondant à la clé étrangère
   @belongsTo(() => Section)
   public section: ReturnType<typeof belongsTo> // Relation vers le modèle Section
-
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
-
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 }
