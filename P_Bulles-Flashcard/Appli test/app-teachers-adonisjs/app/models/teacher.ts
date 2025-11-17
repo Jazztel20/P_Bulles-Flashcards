924c76413ecba7dca86d6b64a511e3618a2eb4a7
Jazztel20 <pb58unc@eduvaud.ch>
2025-11-17 09:07:07 +0100

    fix(ImportType): correction d'une erreur dans le code du step 3

--- P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/app/models/teacher.ts
+++ P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/app/models/teacher.ts
@@ -1,28 +1,41 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, belongsTo, BelongsTo } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
 import Section from '#models/section'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Teacher extends BaseModel {
   // Renommer le nom de la table pour respecter les conventions de nommage de l'ETML
   public static table = 't_teacher'
+
   @column({ isPrimary: true })
   declare id: number
+
   @column()
-  declare gender: String
+  declare gender: string
+
   @column()
-  declare firstname: String
+  declare firstname: string
+
   @column()
-  declare lastname: String
+  declare lastname: string
+
   @column()
-  declare nickname: String
+  declare nickname: string
+
   @column()
-  declare origine: String
+  declare origine: string
+
+  // Colonne correspondant à la clé étrangère
   @column()
-  declare sectionId: number // Colonne correspondant à la clé étrangère
+  declare sectionId: number
+
+  // Relation vers le modèle Section
   @belongsTo(() => Section)
-  public section: ReturnType<typeof belongsTo> // Relation vers le modèle Section
+  declare section: BelongsTo<typeof Section>
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
+
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 }
