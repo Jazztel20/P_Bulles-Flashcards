fd6a67c1d68c7625ab1b3f48289735070e358f1f
Jazztel20 <pb58unc@eduvaud.ch>
2025-11-24 09:21:14 +0100

    fix(kernel.ts): modification du code du kernel.ts afin de féfinir le middleware après le middleware qui initialise auth

--- P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/start/kernel.ts
+++ P_Bulles-Flashcard/Appli test/app-teachers-adonisjs/start/kernel.ts
@@ -7,16 +7,13 @@
 | or the router.
 |
 */
-
 import router from '@adonisjs/core/services/router'
 import server from '@adonisjs/core/services/server'
-
 /**
  * The error handler is used to convert an exception
- * to an HTTP response.
+ * to a HTTP response.
  */
 server.errorHandler(() => import('#exceptions/handler'))
-
 /**
  * The server middleware stack runs middleware on all the HTTP
  * requests, even if there is no route registered for
@@ -27,7 +24,6 @@ server.use([
   () => import('@adonisjs/static/static_middleware'),
   () => import('@adonisjs/vite/vite_middleware'),
 ])
-
 /**
  * The router middleware stack runs middleware on all the HTTP
  * requests with a registered route.
@@ -36,14 +32,15 @@ router.use([
   () => import('@adonisjs/core/bodyparser_middleware'),
   () => import('@adonisjs/session/session_middleware'),
   () => import('@adonisjs/shield/shield_middleware'),
-  () => import('@adonisjs/auth/initialize_auth_middleware')
+  () => import('@adonisjs/auth/initialize_auth_middleware'),
+  () => import('#middleware/share_auth_state_middleware'),
 ])
-
 /**
  * Named middleware collection must be explicitly assigned to
  * the routes or the routes group.
  */
 export const middleware = router.named({
+  ensureAdmin: () => import('#middleware/ensure_admin_middleware'),
   guest: () => import('#middleware/guest_middleware'),
-  auth: () => import('#middleware/auth_middleware')
+  auth: () => import('#middleware/auth_middleware'),
 })
