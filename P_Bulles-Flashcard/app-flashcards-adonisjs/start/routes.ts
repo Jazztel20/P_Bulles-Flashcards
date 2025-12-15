7e9966cf84013ec771f373df0319a8859d2cceda
Jazztel20 <pb58unc@eduvaud.ch>
2025-12-15 10:02:53 +0100

    feat(route update): création de la route pour modifier un deck

--- P_Bulles-Flashcard/app-flashcards-adonisjs/start/routes.ts
+++ P_Bulles-Flashcard/app-flashcards-adonisjs/start/routes.ts
@@ -17,5 +17,8 @@ router.get('/decks/create', [DecksController, 'create']).as('decks.create')
 router.post('/decks', [DecksController, 'store']).as('decks.store')
 
 router.get('/decks/:id', [DecksController, 'show']).as('decks.show')
+
 router.get('/decks/:id/edit', [DecksController, 'edit']).as('decks.edit')
+router.put('/decks/:id', [DecksController, 'update']).as('decks.update')
+
 router.delete('/decks/:id', [DecksController, 'destroy']).as('decks.destroy')
