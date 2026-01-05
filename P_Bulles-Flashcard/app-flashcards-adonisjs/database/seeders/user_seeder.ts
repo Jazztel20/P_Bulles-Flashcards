c25cdbbc0434a451f396745134a984f3cf84da09
Jazztel20 <pb58unc@eduvaud.ch>
2026-01-05 08:38:21 +0100

    fix(user_seeder): problème d'indentation reglé

--- P_Bulles-Flashcard/app-flashcards-adonisjs/database/seeders/user_seeder.ts
+++ P_Bulles-Flashcard/app-flashcards-adonisjs/database/seeders/user_seeder.ts
@@ -1,11 +1,11 @@
 import User from '#models/user'
-import { BaseSeeder } from '@adonisjs/lucid/seeders' 
-export default class extends BaseSeeder { 
-async run() { 
-// Création de users 
-await User.createMany([ 
-      { username: 'Albert', password: 'user', isAdmin: false }, 
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+export default class extends BaseSeeder {
+  async run() {
+    // Création de users
+    await User.createMany([
+      { username: 'Albert', password: 'user', isAdmin: false },
       { username: 'Edouard', password: 'admin', isAdmin: true },
-    ]) 
-  } 
-} 
+    ])
+  }
+}
